9e51d3ba717de84ebd6a3466f074be8f4f3e9b8d
gkavin57 <gkavin77@gmail.com>
2023-01-12 12:17:37 +0530

    cors

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ import { MongoClient } from "mongodb";
 import moviesRouter from "./routes/movies.route.js";
 import userRouter from "./routes/user.router.js";
 app.use(express.json()); //1.request middleware vantu apm thaa function move aaagum,middleware(inbuild) converts the body json to normal js object
-
+import cors from "cors";
+app.use(cors());
 import * as dotenv from "dotenv";
 dotenv.config();
 //env-environment variables-env
@@ -135,30 +136,24 @@ app.get("/", (req, res) => {
 app.use("/movies", moviesRouter);
 app.use("/user", userRouter);
 
-const mobiles = [
-  {
-    model: "OnePlus 9 5G",
-    img: "https://m.media-amazon.com/images/I/61fy+u9uqPL._SX679_.jpg",
-    company: "Oneplus",
-  },
-  {
-    model: "Iphone 13 mini",
-    img: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-13-mini-blue-select-2021?wid=470&hei=556&fmt=jpeg&qlt=95&.v=1645572315986",
-    company: "Apple",
-  },
-  {
-    model: "Samsung s21 ultra",
-    img: "https://m.media-amazon.com/images/I/81kfA-GtWwL._SY606_.jpg",
-    company: "Samsung",
-  },
-  {
-    model: "Xiomi mi 11",
-    img: "https://m.media-amazon.com/images/I/51K4vNxMAhS._AC_SX522_.jpg",
-    company: "Xiomi",
-  },
-];
-app.get("/mobiles", (request, response) => {
+app.get("/mobiles", async (request, response) => {
+  const mobiles = await client
+    .db("B40wd")
+    .collection("mobiles")
+    .find({})
+    .toArray();
+
   response.send(mobiles);
+});
+
+app.post("/mobiles", async (request, response) => {
+  const data = request.body;
+  const result = await client
+    .db("B40wd")
+    .collection("mobiles")
+    .insertMany(data);
+
+  response.send(result);
 });
 
 const PORT = process.env.PORT; //-auto assign port
